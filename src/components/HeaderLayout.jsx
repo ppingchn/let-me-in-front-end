a21419f273a5bd43fca2d854c4bb64df48fb642b
kanm4s <kanm4s@gmail.com>
2022-06-20 20:06:57 +0700

    config default

---
+++ src/components/HeaderLayout.jsx
@@ -0,0 +1,10 @@
+import { Outlet } from "react-router-dom";
+
+export default function HeaderLayout() {
+  return (
+    <>
+      <div>HeaderLayout</div>
+      <Outlet />
+    </>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+
+    extend: {
+      colors: {
+        blue: "#0466BF",
+        darkgray: "6C6C6C",
+        white: "FFFFFF",
+        gray: "F3F2EF",
+      },
+    },
+  },
+  plugins: [],
+};
