871a615f8a703aecbd5b1028f82fce7d1ec70e9f
paruj_lab <paruj.lab@gmail.com>
2022-06-23 15:01:40 +0700

    feat : signup user

--- src/components/informationForm/WorkExperience.jsx
+++ src/components/informationForm/WorkExperience.jsx
@@ -120,29 +120,8 @@ function WorkExperience({
           </div>
         </div>
 
-        <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-          <label
-            htmlFor="about"
-            className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
-          >
-            About
-          </label>
-          <div className="mt-1 sm:mt-0 sm:col-span-2">
-            <textarea
-              id="about"
-              name="about"
-              rows={6}
-              className="max-w-lg shadow-sm block w-full focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border border-gray-500 rounded-md"
-              defaultValue={""}
-              onChange={handleChangeWorkDescription}
-            />
-            <p className="mt-2 text-sm text-gray-500">
-              Write a few sentences about yourself.
-            </p>
-          </div>
-        </div>
 
-        <div className="sm:border-t sm:border-gray-00"></div>
+        {/* <div className="sm:border-t sm:border-gray-00"></div> */}
       </div>
     </>
   );
