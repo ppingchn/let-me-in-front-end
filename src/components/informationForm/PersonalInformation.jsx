ab83d9225f155b58d5e1badf8d8c636c725cbef1
ppingchn <choonhanunt@gmail.com>
2022-06-28 17:27:05 +0700

    feat : create component to set state value

--- src/components/informationForm/PersonalInformation.jsx
+++ src/components/informationForm/PersonalInformation.jsx
@@ -13,6 +13,16 @@ function PersonalInformation({
   setUsername,
   setPassword,
   setConfirmPassword,
+  setCompanyName,
+  setWebsiteLink,
+  setOverView,
+  setLocation,
+  setCountry,
+  setHouseNumber,
+  setSubDistrict,
+  setDistrict,
+  setProvince,
+  setPostalCode,
 }) {
   let location = useLocation();
 
@@ -31,122 +41,6 @@ function PersonalInformation({
         </p>
       </div>
       <div className="space-y-6 sm:space-y-5">
-        <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-          <label
-            htmlFor="first-name"
-            className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
-          >
-            First name
-          </label>
-          <div className="mt-1 sm:mt-0 sm:col-span-2">
-            <input
-              type="text"
-              name="first-name"
-              id="first-name"
-              autoComplete="given-name"
-              onChange={(e) => setFirstName(e.target.value)}
-              className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-500 rounded-md"
-            />
-          </div>
-        </div>
-
-        <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-          <label
-            htmlFor="last-name"
-            className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
-          >
-            Last name
-          </label>
-          <div className="mt-1 sm:mt-0 sm:col-span-2">
-            <input
-              type="text"
-              name="last-name"
-              id="last-name"
-              autoComplete="family-name"
-              onChange={(e) => setLastName(e.target.value)}
-              className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-500 rounded-md"
-            />
-          </div>
-        </div>
-
-        {/* Gender for only user */}
-        {location.pathname === '/signup' && (
-          <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-            <label
-              htmlFor="country"
-              className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
-            >
-              Gender
-            </label>
-            <div className="mt-1 sm:mt-0 sm:col-span-2">
-              <select
-                id="country"
-                name="country"
-                autoComplete="country-name"
-                onChange={(e) => setGender(e.target.value)}
-                className="max-w-lg block focus:ring-indigo-500 focus:border-indigo-500 w-full shadow-sm sm:max-w-xs sm:text-sm border-gray-500 rounded-md"
-              >
-                <option>Male</option>
-                <option>Female</option>
-              </select>
-            </div>
-          </div>
-        )}
-
-        <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-          <label
-            htmlFor="birthDate"
-            className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
-          >
-            Birth date
-          </label>
-          <div className="mt-1 sm:mt-0 sm:col-span-2">
-            <DatePicker
-              selected={birthDate}
-              onChange={(date: Date) => setBirthDate(date)}
-              className="border-gray-500 rounded-md"
-            />
-          </div>
-        </div>
-
-        <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-          <label
-            htmlFor="email"
-            className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
-          >
-            Email address
-          </label>
-          <div className="mt-1 sm:mt-0 sm:col-span-2">
-            <input
-              id="email"
-              name="email"
-              type="email"
-              autoComplete="email"
-              onChange={(e) => setEmail(e.target.value)}
-              className="block max-w-lg w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-500 rounded-md"
-            />
-          </div>
-        </div>
-
-        <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-          <label
-            htmlFor="phoneNumber"
-            className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
-          >
-            Phone number
-          </label>
-          <div className="mt-1 sm:mt-0 sm:col-span-2">
-            <input
-              type="text"
-              name="phoneNumber"
-              id="phoneNumber"
-              autoComplete="family-name"
-              onChange={(e) => setPhoneNumber(e.target.value)}
-              className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-500 rounded-md"
-            />
-          </div>
-        </div>
-
         <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
           <label
             htmlFor="username"
@@ -203,22 +97,198 @@ function PersonalInformation({
             />
           </div>
         </div>
+        {location.pathname === '/signup' && (
+          <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+            <label
+              htmlFor="first-name"
+              className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+            >
+              First name
+            </label>
+            <div className="mt-1 sm:mt-0 sm:col-span-2">
+              <input
+                type="text"
+                name="first-name"
+                id="first-name"
+                autoComplete="given-name"
+                onChange={(e) => setFirstName(e.target.value)}
+                className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-500 rounded-md"
+              />
+            </div>
+          </div>
+        )}
+
+        {location.pathname === '/signup' && (
+          <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+            <label
+              htmlFor="last-name"
+              className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+            >
+              Last name
+            </label>
+            <div className="mt-1 sm:mt-0 sm:col-span-2">
+              <input
+                type="text"
+                name="last-name"
+                id="last-name"
+                autoComplete="family-name"
+                onChange={(e) => setLastName(e.target.value)}
+                className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-500 rounded-md"
+              />
+            </div>
+          </div>
+        )}
+
+        {/* Gender for only user */}
+        {location.pathname === '/signup' && (
+          <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+            <label
+              htmlFor="country"
+              className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+            >
+              Gender
+            </label>
+            <div className="mt-1 sm:mt-0 sm:col-span-2">
+              <select
+                id="country"
+                name="country"
+                autoComplete="country-name"
+                onChange={(e) => setGender(e.target.value)}
+                className="max-w-lg block focus:ring-indigo-500 focus:border-indigo-500 w-full shadow-sm sm:max-w-xs sm:text-sm border-gray-500 rounded-md"
+              >
+                <option>Male</option>
+                <option>Female</option>
+              </select>
+            </div>
+          </div>
+        )}
+
+        {location.pathname === '/signup' && (
+          <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+            <label
+              htmlFor="birthDate"
+              className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+            >
+              Birth date
+            </label>
+            <div className="mt-1 sm:mt-0 sm:col-span-2">
+              <DatePicker
+                selected={birthDate}
+                onChange={(date: Date) => setBirthDate(date)}
+                className="border-gray-500 rounded-md"
+              />
+            </div>
+          </div>
+        )}
+
+        <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+          >
+            Email address
+          </label>
+          <div className="mt-1 sm:mt-0 sm:col-span-2">
+            <input
+              id="email"
+              name="email"
+              type="email"
+              autoComplete="email"
+              onChange={(e) => setEmail(e.target.value)}
+              className="block max-w-lg w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-500 rounded-md"
+            />
+          </div>
+        </div>
+
+        <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+          <label
+            htmlFor="phoneNumber"
+            className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+          >
+            Phone number
+          </label>
+          <div className="mt-1 sm:mt-0 sm:col-span-2">
+            <input
+              type="text"
+              name="phoneNumber"
+              id="phoneNumber"
+              autoComplete="family-name"
+              onChange={(e) => setPhoneNumber(e.target.value)}
+              className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-500 rounded-md"
+            />
+          </div>
+        </div>
+        {location.pathname === '/signupCompany' && (
+          <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+            <label
+              htmlFor="last-name"
+              className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+            >
+              Company name
+            </label>
+            <div className="mt-1 sm:mt-0 sm:col-span-2">
+              <input
+                type="text"
+                name="last-name"
+                id="last-name"
+                autoComplete="family-name"
+                onChange={(e) => setCompanyName(e.target.value)}
+                className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-500 rounded-md"
+              />
+            </div>
+          </div>
+        )}
+        {location.pathname === '/signupCompany' && (
+          <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+            <label
+              htmlFor="last-name"
+              className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+            >
+              Website Link
+            </label>
+            <div className="mt-1 sm:mt-0 sm:col-span-2">
+              <input
+                type="text"
+                name="last-name"
+                id="last-name"
+                autoComplete="family-name"
+                onChange={(e) => setWebsiteLink(e.target.value)}
+                className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-500 rounded-md"
+              />
+            </div>
+          </div>
+        )}
 
         <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
           <label
             htmlFor="about"
             className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
           >
-            About
+            {location.pathname === '/signup'
+              ? 'About'
+              : location.pathname === '/signupCompany'
+              ? 'Overview'
+              : null}
           </label>
           <div className="mt-1 sm:mt-0 sm:col-span-2">
-            <textarea
-              id="about"
-              name="about"
-              rows={6}
-              className="max-w-lg shadow-sm block w-full focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border border-gray-500 rounded-md"
-              defaultValue={''}
-            />
+            {location.pathname === '/signup' ? (
+              <textarea
+                id="about"
+                name="about"
+                rows={6}
+                className="max-w-lg shadow-sm block w-full focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border border-gray-500 rounded-md"
+                defaultValue={''}
+              />
+            ) : location.pathname === '/signupCompany' ? (
+              <textarea
+                id="about"
+                name="about"
+                onChange={(e) => setOverView(e.target.value)}
+                rows={6}
+                className="max-w-lg shadow-sm block w-full focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border border-gray-500 rounded-md"
+                defaultValue={''}
+              />
+            ) : null}
             <p className="mt-2 text-sm text-gray-500">
               Write a few sentences about yourself.
             </p>
@@ -236,25 +306,24 @@ function PersonalInformation({
             Use a permanent address where you can receive mail.
           </p>
         </div>
+        {/* LongDo Map at here */}
 
         <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
           <label
-            htmlFor="country"
+            htmlFor="street-address"
             className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
           >
             Country
           </label>
           <div className="mt-1 sm:mt-0 sm:col-span-2">
-            <select
-              id="country"
-              name="country"
-              autoComplete="country-name"
-              className="max-w-lg block focus:ring-indigo-500 focus:border-indigo-500 w-full shadow-sm sm:max-w-xs sm:text-sm border-gray-500 rounded-md"
-            >
-              <option>United States</option>
-              <option>Canada</option>
-              <option>Mexico</option>
-            </select>
+            <input
+              type="text"
+              name="street-address"
+              id="street-address"
+              autoComplete="street-address"
+              onChange={(e) => setCountry(e.target.value)}
+              className="block max-w-lg w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-500 rounded-md"
+            />
           </div>
         </div>
 
@@ -263,13 +332,14 @@ function PersonalInformation({
             htmlFor="street-address"
             className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
           >
-            Street address
+            House Number
           </label>
           <div className="mt-1 sm:mt-0 sm:col-span-2">
             <input
               type="text"
               name="street-address"
               id="street-address"
+              onChange={(e) => setHouseNumber(e.target.value)}
               autoComplete="street-address"
               className="block max-w-lg w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-500 rounded-md"
             />
@@ -281,13 +351,32 @@ function PersonalInformation({
             htmlFor="city"
             className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
           >
-            City
+            Sub-district
           </label>
           <div className="mt-1 sm:mt-0 sm:col-span-2">
             <input
               type="text"
               name="city"
               id="city"
+              onChange={(e) => setSubDistrict(e.target.value)}
+              autoComplete="address-level2"
+              className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-500 rounded-md"
+            />
+          </div>
+        </div>
+        <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+          <label
+            htmlFor="city"
+            className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+          >
+            District
+          </label>
+          <div className="mt-1 sm:mt-0 sm:col-span-2">
+            <input
+              type="text"
+              name="city"
+              id="city"
+              onChange={(e) => setDistrict(e.target.value)}
               autoComplete="address-level2"
               className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-500 rounded-md"
             />
@@ -299,7 +388,7 @@ function PersonalInformation({
             htmlFor="region"
             className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
           >
-            State / Province
+            Province
           </label>
           <div className="mt-1 sm:mt-0 sm:col-span-2">
             <input
@@ -307,6 +396,7 @@ function PersonalInformation({
               name="region"
               id="region"
               autoComplete="address-level1"
+              onChange={(e) => setProvince(e.target.value)}
               className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-500 rounded-md"
             />
           </div>
@@ -324,6 +414,7 @@ function PersonalInformation({
               type="text"
               name="postal-code"
               id="postal-code"
+              onChange={(e) => setPostalCode(e.target.value)}
               autoComplete="postal-code"
               className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-500 rounded-md"
             />
