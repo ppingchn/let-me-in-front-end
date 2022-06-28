7c425ef320d2a7784f868775f1b42fbd65d6baeb
ppingchn <choonhanunt@gmail.com>
2022-06-28 17:26:20 +0700

    feat : sorting state variable and register fn

--- src/components/page/SignupCompanyPage.jsx
+++ src/components/page/SignupCompanyPage.jsx
@@ -5,8 +5,11 @@ import { useState } from 'react';
 import Profile from '../informationForm/Profile';
 import PersonalInformation from '../informationForm/PersonalInformation';
 import WorkExperience from '../informationForm/WorkExperience';
+import { useAuth } from '../../context/authContext';
 
 export default function SignupCompanyPage() {
+  // useAuthContext
+  const { register } = useAuth();
   //useState
 
   //This Page
@@ -18,27 +21,12 @@ export default function SignupCompanyPage() {
   const [coverPhoto, setCoverPhoto] = useState('');
 
   // Personal Information
-  const [firstname, setFirstName] = useState('');
-  const [lastname, setLastName] = useState('');
-  const [gender, setGender] = useState('');
+  const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
-  const [birthDate, setBirthDate] = useState(new Date());
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
   const [about, setAbout] = useState('');
-
-  //Work experience Information
-  const [companyArray, setCompanyArray] = useState([
-    {
-      index: 0,
-      companyName: '',
-      position: '',
-      startDate: '',
-      endDate: '',
-      workDescription: '',
-    },
-  ]);
 
   const [position, setPositon] = useState('');
   const [workDescription, setWorkDescription] = useState('');
@@ -48,14 +36,15 @@ export default function SignupCompanyPage() {
 
   //Company Detail
   const [companyName, setCompanyName] = useState('');
-  const [websiteName, setWebsiteName] = useState('');
+  const [websiteLink, setWebsiteLink] = useState('');
   const [overView, setOverView] = useState('');
   const [location, setLocation] = useState(null);
 
   //Address Information
   const [country, setCountry] = useState('');
-  const [stressAddress, setStressAddress] = useState('');
-  const [city, setCity] = useState('');
+  const [houseNumber, setHouseNumber] = useState('');
+  const [subDistrict, setSubDistrict] = useState('');
+  const [district, setDistrict] = useState('');
   const [province, setProvince] = useState('');
   const [postalCode, setPostalCode] = useState('');
 
@@ -64,68 +53,34 @@ export default function SignupCompanyPage() {
   //Additional
   let navigate = useNavigate();
 
-  const handleClickIncrease = (e) => {
-    e.preventDefault();
-    setCount(count + 1);
-    setCountArray([...countArray, count]);
-
-    // console.log(count)
-
-    let clone = [...companyArray];
-    const newCloneArray = {
-      index: count,
-      companyName: '',
-      position: '',
-      startDate: '',
-      endDate: '',
-      workDescription: '',
-    };
-    setCompanyArray([...clone, newCloneArray]);
-    // console.log(countArray);
-    console.log(companyArray);
-    console.log('first');
-  };
-
-  console.log(companyArray);
-
-  const handleClickDecrease = (e) => {
-    e.preventDefault();
-    if (count > 0) {
-      setCount(count - 1);
-    }
-    const cloneCountArray = [...countArray];
-    cloneCountArray.pop();
-    setCountArray(cloneCountArray);
-    console.log(countArray);
-    console.log('second');
-  };
-  // console.log(countArray);
-
   const handleSubmitSignUp = async (e) => {
     try {
       e.preventDefault();
       //validate input first
+      const address = {
+        country,
+        houseNumber,
+        subDistrict,
+        district,
+        province,
+        postalCode,
+      };
 
-      // setLoading(true)
-      // //end validate
-      // await signUp({
-      //   firstName,
-      //   lastName,
-      //   email,
-      //   phoneNumber,
-      //   password,
-      //   confirmPassword,
-      //   profilePic,
-      //   addressName,
-      //   address,
-      //   city,
-      //   district,
-      //   postalCode,
-      //   moreDetails,
-      // });
-      // console.log(profilePic)
-
-      // setLoading(false)
+      const companyData = new FormData();
+      companyData.append('role', 'company');
+      companyData.append('username', username);
+      companyData.append('password', password);
+      companyData.append('confirmPassword', confirmPassword);
+      companyData.append('profilePic', profilePic);
+      companyData.append('coverPic', coverPhoto);
+      companyData.append('email', email);
+      companyData.append('phoneNumber', phoneNumber);
+      companyData.append('websiteLink', websiteLink);
+      companyData.append('address', address);
+      companyData.append('companyName', companyName);
+      companyData.append('overview', overView);
+      companyData.append('location', location);
+      await register(companyData);
       navigate('/');
     } catch (err) {
       // setError(err.response.data.message);
@@ -150,61 +105,29 @@ export default function SignupCompanyPage() {
               />
 
               <PersonalInformation
-                setStartBirthDate={setBirthDate}
-                birthDate={birthDate}
+                setUsername={setUsername}
+                setPassword={setPassword}
+                setConfirmPassword={setConfirmPassword}
+                setEmail={setEmail}
+                setPhoneNumber={setPhoneNumber}
+                setCompanyName={setCompanyName}
+                setWebsiteLink={setWebsiteLink}
+                setOverView={setOverView}
+                setLocation={setLocation}
+                setCountry={setCountry}
+                setHouseNumber={setHouseNumber}
+                setSubDistrict={setSubDistrict}
+                setDistrict={setDistrict}
+                setProvince={setProvince}
+                setPostalCode={setPostalCode}
               />
-
-              <div className="pt-8 space-y-6 sm:pt-10 sm:space-y-5">
-                <div className="flex justify-between text-end">
-                  <h3 className="text-lg leading-6 font-medium text-gray-900">
-                    Work Experience
-                  </h3>
-                  <div>
-                    {/* plus */}
-                    <button
-                      onClick={
-                        // (e) => {
-                        // e.preventDefault();
-                        // setCount(count + 1);
-                        handleClickIncrease
-                        // }
-                      }
-                    >
-                      <i className="fa-solid fa-plus"></i>
-                    </button>
-                    {/* minus */}
-                    {count > 1 && (
-                      <button onClick={handleClickDecrease}>
-                        <i className="ml-2 fa-solid fa-minus" />
-                      </button>
-                    )}
-                  </div>
-                </div>
-
-                {countArray.map((el, idx) => {
-                  return (
-                    <WorkExperience
-                      key={idx}
-                      index={idx}
-                      companyArray={companyArray}
-                      setCompanyArray={setCompanyArray}
-                      setPositon={setPositon}
-                      // workDescription={workDescription}
-                      setWorkDescription={setWorkDescription}
-                      setStartDate={setStartDate}
-                      startDate={startDate}
-                      setEndDate={setEndDate}
-                      endDate={endDate}
-                    />
-                  );
-                })}
-              </div>
             </div>
 
             <div className="pt-5">
               <div className="flex justify-end">
                 <button
                   type="button"
+                  onClick={() => navigate('/')}
                   className="bg-white py-2 px-4 border border-gray-500 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
                   Cancel
