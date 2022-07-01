40a4c54dbad3ffaabaa6f2ca9a3d52231c8ea45d
ppingchn <choonhanunt@gmail.com>
2022-07-01 22:24:02 +0700

    refactor : megre classname

--- src/components/Jobs/Sidebars/ApplicationSetting/UploadResume.jsx
+++ src/components/Jobs/Sidebars/ApplicationSetting/UploadResume.jsx
@@ -168,11 +168,10 @@ function UploadResume({ resume, defaultresume, onChange, onDelete }) {
                     viewBox="0 0 24 24"
                     data-supported-dps="24x24"
                     fill="currentColor"
-                    className="mercado-match"
                     width="35"
                     height="35"
                     focusable="false"
-                    className="hover:bg-gray-200 px-2 py-2 rounded-full"
+                    className=" mercado-match hover:bg-gray-200 px-2 py-2 rounded-full"
                   >
                     <path d="M13.42 12L20 18.58 18.58 20 12 13.42 5.42 20 4 18.58 10.58 12 4 5.42 5.42 4 12 10.58 18.58 4 20 5.42z"></path>
                   </svg>
