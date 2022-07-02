328df4be624efbd8fc9ac5a340afdad3cabff7eb
ppingchn <choonhanunt@gmail.com>
2022-07-02 14:42:06 +0700

    feat : dropdown and function

---
+++ src/components/Reply/EditReplyDropdown.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Menu } from '@headlessui/react';
+import { BsThreeDots } from 'react-icons/bs';
+import { HiTrash } from 'react-icons/hi';
+import { MdModeEditOutline } from 'react-icons/md';
+
+function EditReplyDropdown({ setOpen, setDelete }) {
+  return (
+    <Menu as="div" className="relative inline-block text-left">
+      <div>
+        <Menu.Button className="flex justify-center items-center w-10 h-10 rounded-full shadow-sm text-sm font-medium text-gray-700 hover:bg-gray transition-all focus:outline-none ">
+          <BsThreeDots className="text-2xl" aria-hidden="true" />
+        </Menu.Button>
+      </div>
+      <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <div className="flex flex-col justify-around my-2">
+          <Menu.Item>
+            <button
+              className="flex gap-2 items-center hover:bg-gray px-5 py-4"
+              onClick={() => setOpen(true)}
+            >
+              <MdModeEditOutline size="20" />
+              <span>Edit</span>
+            </button>
+          </Menu.Item>
+          <Menu.Item>
+            <button
+              className="flex gap-2 items-center hover:bg-gray px-5 py-4"
+              onClick={() => setDelete()}
+            >
+              <HiTrash size="20" />
+              <span>Delete</span>
+            </button>
+          </Menu.Item>
+        </div>
+      </Menu.Items>
+    </Menu>
+  );
+}
+
+export default EditReplyDropdown;
