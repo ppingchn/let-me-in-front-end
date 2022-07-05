9ae056673858428d05c49640d77c7da9ca78a27c
dolphinza8 <dolphinza8nj@gmail.com>
2022-07-05 21:36:57 +0700

    notification

--- src/components/notification/NotificationElement.jsx
+++ src/components/notification/NotificationElement.jsx
@@ -1,36 +1,31 @@
 import { Fragment } from 'react';
 import { Menu, Transition } from '@headlessui/react';
 import { BsThreeDots } from 'react-icons/bs';
+import moment from 'moment';
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ');
 }
 
-export default function NotificationElement({ role }) {
+export default function NotificationElement({ notification }) {
+  const time = moment(notification.createdAt).fromNow();
   return (
     <div className="flex w-full gap-3 items-center px-5 py-4 border-b-[1px] border-slate-200 hover:bg-gray">
-      {role === 'user' ? (
-        <img
-          className="inline-block h-12 w-12 sm:h-12 sm:w-12 rounded-full cursor-pointer"
-          src="https://media-exp1.licdn.com/dms/image/C5635AQFLYHLx2MfUTg/profile-framedphoto-shrink_400_400/0/1599535039224?e=1656594000&v=beta&t=O9y8eKEW1-wXS4dTovWyH7Nd17POXUuH0WvZFgFGLfA"
-          alt=""
-        />
-      ) : (
-        <img
-          className="inline-block h-12 w-12 sm:h-12 sm:w-12 cursor-pointer"
-          src="https://media-exp1.licdn.com/dms/image/C560BAQEn3FvAF4Gpjg/company-logo_200_200/0/1654087026909?e=1664409600&v=beta&t=NTO3cCLbdpI5Q7OLiSHPusrPeo5BiJFNhjv9iYQYsYA"
-          alt=""
-        />
-      )}
+      <img
+        className="inline-block h-12 w-12 sm:h-12 sm:w-12 rounded-full cursor-pointer"
+        src={notification.User.profilePic}
+        alt=""
+      />
+
       {/* content */}
       <span className="w-full text-sm">
-        {`Welcome to the Wrap-Up, your summary of the day's top news and talking
-        points, curated by LinkedIn News Asia. Today, we ask: Is there a quote
-        that motivates you at work?`.length > 100}
+        {notification.PostId && notification.Post.detail}
+        {notification.CommentId && notification.Comment.title}
+        {notification.JobListId && notification.JobList.jobDescription}
       </span>
       {/* dropdown */}
       <div className="w-10 flex flex-col items-end">
-        <span className="text-sm text-darkgray relative right-2">3h</span>
+        <span className="text-sm text-darkgray relative right-2">{time}</span>
         <Menu as="div" className="relative inline-block text-left">
           <div>
             <Menu.Button className="flex justify-center items-center w-10 h-10 rounded-full shadow-sm bg-white text-sm font-medium text-gray-700 hover:bg-gray transition-all focus:outline-none ">
